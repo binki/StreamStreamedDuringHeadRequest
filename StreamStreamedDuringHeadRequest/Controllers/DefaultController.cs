a007b0b131cab83ee5742d3b57d18bc8fc2c7520
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2018-08-14 21:30:16 -0400

    Add a simple controller and route.

---
+++ StreamStreamedDuringHeadRequest/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace StreamStreamedDuringHeadRequest.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        [HttpHead]
+        public HttpResponseMessage Get()
+        {
+            var response = Request.CreateResponse();
+            var (stream, encoding) = GetStream();
+            response.Content = new StreamContent(stream);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain")
+            {
+                CharSet = encoding.WebName,
+            };
+            return response;
+        }
+
+        (Stream, Encoding) GetStream()
+        {
+            var ms = new MemoryStream();
+            var encoding = new UTF8Encoding(false, true);
+            using (var writer = new StreamWriter(ms, encoding, 8192, true))
+            {
+                foreach (var i in Enumerable.Range(0, 24))
+                {
+                    writer.WriteLine("All play and no work makes Jack a poor boy.");
+                }
+            }
+            ms.Position = 0;
+            return (ms, encoding);
+        }
+    }
+}
