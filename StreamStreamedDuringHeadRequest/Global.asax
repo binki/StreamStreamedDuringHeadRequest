0e0513f42b4c781ce9b1b938e4319964aff1c66f
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2018-08-14 21:14:52 -0400

    Switch to OWIN.

--- StreamStreamedDuringHeadRequest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StreamStreamedDuringHeadRequest.WebApiApplication" Language="C#" %>
---
+++ StreamStreamedDuringHeadRequest/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(StreamStreamedDuringHeadRequest.Startup))]
+
+namespace StreamStreamedDuringHeadRequest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
